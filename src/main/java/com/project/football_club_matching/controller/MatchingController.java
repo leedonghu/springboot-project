99b37b78357f0008a96d235e2356399adf0c82ef
leedonghu <ldh930517@gmail.com>
2022-08-29 15:58:59 +0900

    8/29 session/cookie with controller

--- src/main/java/com/project/football_club_matching/controller/MatchingController.java
+++ src/main/java/com/project/football_club_matching/controller/MatchingController.java
@@ -2,6 +2,9 @@ package com.project.football_club_matching.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,33 +17,54 @@ import com.project.football_club_matching.entity.Member;
 import com.project.football_club_matching.entity.Question;
 import com.project.football_club_matching.entity.QuestionResult;
 import com.project.football_club_matching.service.MatchingService;
+import com.project.football_club_matching.session.SessionManager;
 
 @Controller
 public class MatchingController {
     
     @Autowired MatchingService matchingService;
+    @Autowired SessionManager sessionManager;
 
     @RequestMapping(value = "/home", method = RequestMethod.GET)
-    public String home(Model model){
+    public String home(Model model, HttpServletResponse response, HttpServletRequest request){
         
-        Member member = new Member();
-        matchingService.saveMember(member);
+        Member checkMember = (Member) sessionManager.getSession(request);
         List<Question> list = matchingService.findQuestions();
-        
+        if(checkMember == null){
 
-        model.addAttribute("id", member.getId());
-        model.addAttribute("questions", list);
+            Member member = new Member();
+            sessionManager.createSession(member, response);
+            matchingService.saveMember(member);
+            model.addAttribute("id", member.getId());
+            model.addAttribute("questions", list);
+            System.out.println("세션 생성");
+        }else{
+            model.addAttribute("id", checkMember.getId());
+            model.addAttribute("questions", list);
+            System.out.println("세션 이미 있음");
+
+        }
+        
         return "home";
     }
 
     @RequestMapping(value = "/home/{id}", method = RequestMethod.POST)
-    public String getData(QuestionResult questionResult, Model model, @PathVariable("id") String id){
-        
+    public String getData(QuestionResult questionResult, Model model, @PathVariable("id") String id, HttpServletRequest request){
+        Member checkMember = (Member) sessionManager.getSession(request);
+        if(checkMember == null){
+            return "home";
+        }
         
         String result = matchingService.getResult(questionResult, Long.parseLong(id));
         
 
         model.addAttribute("result", result);
         return "result";
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout(HttpServletRequest request){
+        sessionManager.expire(request);
+        return "redirect:/home";
     }
 }
